f621c7986194ea33879f8e295f589fbf140a0acd
MustafaGungor <mustafagungormg@outlook.com>
2018-03-24 02:07:30 +0300

    #2 last design today project

--- harmony-web/src/com/harmony/web/WebHarmony.java
+++
@@ -1,7 +0,0 @@
-package com.harmony.web;
-
-public class WebHarmony {
-    public static void main(String[] args) {
-        System.out.println("web Harmony");
-    }
-}
--- harmony-web/src/com/harmony/web/main/HarmonyApplication.java
+++ harmony-web/src/com/harmony/web/main/HarmonyApplication.java
@@ -1,4 +1,4 @@
-package com.harmonyjava.harmony;
+package com.harmony.web.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
