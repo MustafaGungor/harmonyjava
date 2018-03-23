d3b0e3e3774f0a7c76f12bdb3710b6f1d3fc25a1
ucakyunus <yunus.ucak@mebitech.com>
2018-03-24 01:03:46 +0300

    #2 logging-mail-report security moduler added project added package and class

---
+++ harmony-logging/src/com/harmony/logging/LoggingHarmony.java
@@ -0,0 +1,7 @@
+package com.harmony.logging;
+
+public class LoggingHarmony {
+    public static void main(String[] args) {
+        System.out.println("Logging");
+    }
+}
---
+++ harmony-security/src/com/harmony/security/SecurityHarmony.java
@@ -0,0 +1,8 @@
+package com.harmony.security;
+
+public class SecurityHarmony {
+    public static void main(String[] args) {
+        System.out.println("Security");
+    }
+
+}
