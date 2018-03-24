1a1664c02ac49bae0cc2ff9e2138d5eea8b99038
MustafaGungor <mustafagungormg@outlook.com>
2018-03-24 16:38:56 +0300

    #2 last design web app.

--- harmony-web/src/java/com/harmony/web/HarmonyApplication.java
+++ harmony-web/src/java/com/harmony/web/HarmonyApplication.java
@@ -1,11 +1,10 @@
-package com.harmony.web.main;
+package com.harmony.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HarmonyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HarmonyApplication.class, args);
 	}
